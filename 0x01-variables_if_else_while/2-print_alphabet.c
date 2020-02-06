413c439fb8f0352db0b1f8557dd33624c5cae88c
roundhousetally <tahlia.w.roper@gmail.com>
2020-02-06 20:07:16 +0000

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/**
+ *
+ *main - entry
+ *
+ *return: always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a' ; ch <= 'z'; ch++)
+
+	{
+		putchar(ch);
+	}
+	putchar ('\n');
+	return (0);
+}
