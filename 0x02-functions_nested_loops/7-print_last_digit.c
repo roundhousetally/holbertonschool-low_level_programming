087fc12f6a3aafeed31372990272547f1e74023e
roundhousetally <tahlia.w.roper@gmail.com>
2020-02-11 20:57:33 +0000

    Task 7, last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <unistd.h>
+/**
+ *print_last_digit - returns the value of the last digit of a num.
+ *
+ *@l: the number to be checked.
+ *
+ * Return: Last digit
+ *
+ */
+
+int print_last_digit(int l)
+{
+	int n;
+
+	if (l >= '0')
+	{
+		n = l % 10;
+		_putchar(n + '0');
+	}
+	else if (l < '0')
+	{
+		l = l * (-1);
+		n = l % 10;
+		_putchar(n + '0');
+	}
+	return (n);
+}
