324272a001798e99b63887124f9f24098812d315
roundhousetally <tahlia.w.roper@gmail.com>
2020-02-11 16:42:09 +0000

    Task 6, abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *_abs - computes the absolute value of an integer.
+ *
+ *@i: the number to be checked.
+ *
+ * Return: Absolute value
+ *
+ */
+
+int _abs(int i)
+{
+	if (i >= 1)
+	{
+		return (i);
+	}
+	else if (i < 0)
+	{
+		return (i * (-1));
+	}
+	return (0);
+}
