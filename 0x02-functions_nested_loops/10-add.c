d7d96e6b350f7d268bc02809e7f37dc50d4ceeba
roundhousetally <tahlia.w.roper@gmail.com>
2020-02-11 23:53:03 +0000

    Task 10, add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *add- adds two integers together
+ *
+ *@c , @d, the two integers to add
+ *
+ *Return: result
+ *
+ */
+int add(int c, int d)
+{
+	int h;
+	h = c + d;
+	_putchar(h + '0');
+	return (h);
+}
