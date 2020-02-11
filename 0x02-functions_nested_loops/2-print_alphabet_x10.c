f97e54938b18d4c95e73452fe50637d8e1e176b3
roundhousetally <tahlia.w.roper@gmail.com>
2020-02-11 00:03:12 +0000

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - writes the alphabet to stdout 10 times
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void print_alphabet_x10(void)
+{
+	int l = 0;
+
+	while (l < 11)
+	{
+		char ch;
+
+		for (ch = 'a' ; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar ('\n');
+			l++;
+	}
+}
