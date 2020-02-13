aa1dd928bc645d7d10b8adfd07ce4ea2954ae727
roundhousetally <tahlia.w.roper@gmail.com>
2020-02-13 23:48:58 +0000

    Task 9, FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+
+/**
+ *main - entry point, printing numbers 1-100
+ *
+ *except multiples of 3, 5, and 3 & 5- replacing with words.
+ *
+ * Return: On success 0.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a < 100; a++)
+	{
+		if ((a % 3 == 0) && (a % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", a);
+		}
+
+	}
+
+	printf("\n");
+
+}
