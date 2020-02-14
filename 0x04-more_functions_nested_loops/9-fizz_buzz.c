2beb9351ca8ec525f34eadd8e02a7620bb9f0c9e
roundhousetally <tahlia.w.roper@gmail.com>
2020-02-14 00:11:07 +0000

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
  *main - entry point, printing numbers 1-100
  *
@@ -14,27 +13,27 @@ int main(void)
 {
 	int a;
 
-	for (a = 1; a < 100; a++)
+	for (a = 1; a <= 100; a++)
 	{
+		if (a > 1 && a < 101)
+		{
+			printf(" ");
+		}
 		if ((a % 3 == 0) && (a % 5 == 0))
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (a % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if (a % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else
-		{
-			printf("%d ", a);
-		}
+			printf("%d", a);
 
 	}
-
 	printf("\n");
-
 }
