77350f679b13e1013d1a145205bb252f4fe2b013
roundhousetally <tahlia.w.roper@gmail.com>
2020-02-14 03:08:43 +0000

    Task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main- entry point
+ *
+ *
+ *
+ *Return: 0
+ *
+ */
+int main(void)
+{
+	int i;
+	long int n = 612852475143;
+	long int max = -1;
+
+	for (i = 3; i <= sqrt(n); i=i+2)
+	{
+		while(n % i == 0)
+	      {
+		      max = i;
+		      n = n/i;
+	      }
+	}
+	if(n > 2)
+	{
+		max = n;
+	}
+	printf("%ld\n", max);
+
+	return (0);
+}
