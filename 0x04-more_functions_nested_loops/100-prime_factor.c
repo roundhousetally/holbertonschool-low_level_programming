ba0fda330e03682f8c32e8490ec9a450a7771959
roundhousetally <tahlia.w.roper@gmail.com>
2020-02-14 03:11:42 +0000

    Task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -15,15 +15,15 @@ int main(void)
 	long int n = 612852475143;
 	long int max = -1;
 
-	for (i = 3; i <= sqrt(n); i=i+2)
+	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
-		while(n % i == 0)
-	      {
-		      max = i;
-		      n = n/i;
-	      }
+		while (n % i == 0)
+		{
+			max = i;
+			n = n / i;
+		}
 	}
-	if(n > 2)
+	if (n > 2)
 	{
 		max = n;
 	}
