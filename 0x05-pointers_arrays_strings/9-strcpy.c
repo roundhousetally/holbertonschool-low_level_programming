3855c2e2a424f9ff8e7b8f8b9dcd551ebd58ab95
roundhousetally <tahlia.w.roper@gmail.com>
2020-02-19 20:17:24 +0000

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,12 +5,27 @@
  *_strcpy - copies a string pointed to by src
  *
  *@dest: destination
- *@src: 
+ *@src:starting string point
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 char *_strcpy(char *dest, char *src)
 {
-	
+	int p;
+	int i;
+
+	p = 0;
+
+
+	while (src[p] != '\0')
+	{
+		p++;
+	}
+
+	for (i = 0; i < p; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
 }
