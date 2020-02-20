b64498c91078e7e7b05979c6830756afaf171c9d
roundhousetally <tahlia.w.roper@gmail.com>
2020-02-20 21:11:00 +0000

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ *_strncat- concatenates two string taking only an n number from the src
+ *
+ *@n: the number of bytes to take from src.
+ *@dest: the destination of the strings.
+ *@src: the string to be appended.
+ *
+ *Return: the concatenated string
+ *
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+
+	char *together = dest;
+	int i = 0;
+
+	while (*dest)
+	{
+		dest++;
+	}
+	if (*dest != '\0')
+	{
+		dest++;
+	}
+	while (((i + 1) <= n) && src[i] != '\0')
+	{
+		*dest++ = *src++;
+		i++;
+	}
+	*dest = '\0';
+	return (together);
+}
