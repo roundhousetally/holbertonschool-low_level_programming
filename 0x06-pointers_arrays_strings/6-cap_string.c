b0c47621d488f205540b909be043be1dc55f35d7
roundhousetally <tahlia.w.roper@gmail.com>
2020-02-21 03:01:25 +0000

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ *cap_string - capitalizes all words of a string
+ *
+ *@s: the string
+ *
+ *Return: string
+ *
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (0 >= 'a' && 0 <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+		else if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
+		    s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!' ||
+		    s[i] == '?' || s[i] == '"' || s[i] == '('
+		    || s[i] == ')' || s[i] == '{' || s[i] == '}')
+		{
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			{
+				s[i + 1] = s[i + 1] - 32;
+			}
+
+		}
+	}
+	return (s);
+}
