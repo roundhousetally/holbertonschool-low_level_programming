5951115125887b58d1fd66c242ac3d7caccc47e7
roundhousetally <tahlia.w.roper@gmail.com>
2020-02-21 05:13:29 +0000

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,4 +9,26 @@
  */
 char *leet(char *s)
 {
+	int i;
+
+	char *a = a;
+	char *e = e;
+	char *o = o;
+	char *t = t;
+	char *l = l;
+
+
+	for(i = 0; i != '\0'; i++)
+	{
+		if(s[i] == 'a' || s[i] == 'e' || s[i] == 'o'||
+		   s[i] == 't' || s[i] == 'l')
+		{
+			*a = 4;
+			*e = 3;
+			*o = 0;
+			*t = 7;
+			*l = 1;
+		}
+	}
+	return (s);
 }
