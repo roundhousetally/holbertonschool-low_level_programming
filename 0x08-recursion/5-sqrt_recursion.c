6e69ee452f88224055149f8419b5a32691e56791
roundhousetally <tahlia.w.roper@gmail.com>
2020-02-27 20:07:41 +0000

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,20 +4,31 @@
  *_sqrt_recursion - returns the natural sq root of a number
  *
  *@n: the number to calculate sq root
- *
+ *@y: to please betty I had to put this desc. here.
  *Return: the square root, if n does not have a sq root, return -1
  */
+int squarerootfinder(int n, int y);
 int _sqrt_recursion(int n)
 {
-	int x = 2;
-
-	if (n == 0 || n == 1)
+	return (squarerootfinder(n, 1));
+}
+/**
+ *squarerootfinder- function finds the sq root
+ *
+ *@n: the number to check against
+ *@y: exp
+ *
+ *Return: y or -1 if not a sq root
+ */
+int squarerootfinder(int n, int y)
+{
+	if (y * y == n)
 	{
-		return (1);
+		return (y);
 	}
-	else if (n > 1)
+	if (y * y < n)
 	{
-		return (_sqrt_recursion());
+		return (squarerootfinder(n, y + 1));
 	}
 	else
 	{
