36cfd2b94687489f493f969d4149c44ef653ff00
roundhousetally <tahlia.w.roper@gmail.com>
2020-03-03 21:21:38 +0000

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,20 +16,17 @@ char *str_concat(char *s1, char *s2)
 
 	full = e = malloc(_strlen(s1) + _strlen(s2) + 1);
 
-	if (s1 == NULL)
+	if (s1 == NULL || s2 == NULL)
 	{
-		return (NULL);
+		s1 = s2 = "";
 	}
-	if (s2 == NULL)
-	{
-		return (NULL);
-	}
-	for (b = s1; (*e = *b) != '\0'; ++b, ++e)
+       	for (b = s1; (*e = *b) != '\0'; ++b, ++e)
 	{
 	}
 	for (b = s2; (*e = *b) != '\0'; ++b, ++e)
 	{
 	}
+	
 	return (full);
 }
 
