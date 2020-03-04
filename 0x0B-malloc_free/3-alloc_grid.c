a94e11cf47dd5af4e62aeccfc3eeae8c9963d141
roundhousetally <tahlia.w.roper@gmail.com>
2020-03-04 01:44:29 +0000

    Task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -32,7 +32,7 @@ int **alloc_grid(int width, int height)
 		if (array[i] == NULL)
 		{
 			return (NULL);
-			for (i = 0; i < height; i++)
+			for (i = 0; i < width; i++)
 			{
 				free(array[i]);
 			}
