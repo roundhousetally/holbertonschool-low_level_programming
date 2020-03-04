f62d9aaf137a8527a175a0e765f23a01c1d459cc
roundhousetally <tahlia.w.roper@gmail.com>
2020-03-04 04:27:54 +0000

    Header update

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -2,9 +2,10 @@
 #define HOLBERTON_H
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+int _strlen(char *s);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
-char *argstrostr(int ac, char **av);
+char *argstostr(int ac, char **av);
 int _putchar(char c);
 #endif /* HOLBERTON_H */
