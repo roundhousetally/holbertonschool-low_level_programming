0a37ca6e2c4ec32ba8823299a75bb2e6ea940715
roundhousetally <tahlia.w.roper@gmail.com>
2020-03-05 15:59:28 +0000

    Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ *malloc_checked - allocates memory and returns memory to a pointer
+ *
+ *@b: original bytes to be multiplied by size to malloc
+ *
+ *
+ *Return: a pointer to memory space, or exit.
+ */
+void *malloc_checked(unsigned int b)
+{
+	size_t **p;
+
+
+	if (b == 0)
+	{
+		exit(98);
+	}
+
+	p = malloc(sizeof(b) * b);
+
+	if (p == NULL)
+	{
+		free(p);
+		exit(98);
+	}
+	else
+	{
+		return (p);
+	}
+	free(p);
+}
