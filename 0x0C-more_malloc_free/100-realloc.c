94e7db73ecf82cf5c312b72bf6afca5289441350
roundhousetally <tahlia.w.roper@gmail.com>
2020-03-06 02:44:47 +0000

    task 4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ *_realloc - reallocate memory to a new size
+ *
+ *@ptr: pointer to new memory block
+ *@old_size: old size of mem block
+ *@new_size: new size of mem block
+ *
+ *Return: a pointer to new memory block
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (new_size > old_size)
+	{
+		return (ptr);
+		free(ptr);
+	}
+	else if (new_size == old_size)
+	{
+		return (ptr);
+		free(ptr);
+	}
+	else
+	{
+		ptr = malloc(new_size);
+	}
+
+	return (ptr);
+	free(ptr);
+
+}
