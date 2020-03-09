544a5e13af62f48e35fe0aeb4c8e3f928cdfb0fd
roundhousetally <tahlia.w.roper@gmail.com>
2020-03-09 21:04:01 +0000

    Task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,13 +6,12 @@
  *@age: age of dog
  *@owner: dog over
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-struct dog dog_t;
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif /* DOG_H */
