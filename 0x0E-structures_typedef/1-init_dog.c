9804669adcaa1487dbc4d70686b24d2a3bb40824
roundhousetally <tahlia.w.roper@gmail.com>
2020-03-09 18:19:58 +0000

    Task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ *init_dog - initializes the variables of struct dog
+ *
+ *@d: pointer to dog
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner of dog
+ *
+ *Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
