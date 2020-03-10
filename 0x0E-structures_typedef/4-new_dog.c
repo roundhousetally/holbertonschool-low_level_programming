f48ae4345ad4a5a6d433b61c20a33176cd2a027e
roundhousetally <tahlia.w.roper@gmail.com>
2020-03-10 02:10:15 +0000

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,25 +10,44 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
+	dog_t *new;
+	unsigned int o, t, i;
 
 	if (name == NULL || owner == NULL)
 	{
 		return (NULL);
 	}
-
-	new_dog = malloc(sizeof(dog_t));
-
-
-	if (new_dog == NULL)
+	new = malloc(sizeof(dog_t));
+	if (new == NULL)
 	{
-		free(new_dog);
 		return (NULL);
 	}
+	for (o = 0; name[o]; o++)
+		;
+	o++;
+	new->name = malloc(o * sizeof(char));
+	if (new->name == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	for (i = 0; i < o; i++)
+	{
+		new->name[i] = name[i];
+	}
+	new->age = age;
+	for (t = 0; owner[t]; t++)
+		;
+	t++;
+	new->owner = malloc(t * sizeof(char));
+	if (new->owner == NULL)
+	{
+		free(new->name);
+		free(new);
+		return (NULL);
+	}
+	for (i = 0; i < o; i++)
+		new->owner[i] = owner[i];
 
-	new_dog->name = name;
-	new_dog->age = age;
-	new_dog->owner = owner;
-
-	return (new_dog);
+	return (new);
 }
