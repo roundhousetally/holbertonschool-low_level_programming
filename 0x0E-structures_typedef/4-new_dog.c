42c144d84f14585dde04fbb997632dc4de1f9a5c
roundhousetally <tahlia.w.roper@gmail.com>
2020-03-10 02:37:03 +0000

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,7 @@
 #include "dog.h"
 #include <stdlib.h>
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 /**
  *new_dog - creates a new dog
  *@name: name of new dog
@@ -10,44 +12,92 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new;
-	unsigned int o, t, i;
+	dog_t *new_dog;
 
 	if (name == NULL || owner == NULL)
 	{
 		return (NULL);
 	}
-	new = malloc(sizeof(dog_t));
-	if (new == NULL)
-	{
-		return (NULL);
-	}
-	for (o = 0; name[o]; o++)
-		;
-	o++;
-	new->name = malloc(o * sizeof(char));
-	if (new->name == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-	for (i = 0; i < o; i++)
-	{
-		new->name[i] = name[i];
-	}
-	new->age = age;
-	for (t = 0; owner[t]; t++)
-		;
-	t++;
-	new->owner = malloc(t * sizeof(char));
-	if (new->owner == NULL)
-	{
-		free(new->name);
-		free(new);
-		return (NULL);
-	}
-	for (i = 0; i < o; i++)
-		new->owner[i] = owner[i];
 
-	return (new);
+	new_dog = malloc(sizeof(dog_t));
+
+
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+
+	new_dog->name = malloc(_strlen(name) * sizeof(char) + 1);
+
+	_strcpy(new_dog->name, name);
+
+	if (new_dog->name == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+
+	new_dog->owner = malloc(_strlen(owner) * sizeof(char) + 1);
+
+	_strcpy(new_dog->owner, owner);
+
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->owner);
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->age = age;
+	return (new_dog);
+}
+/**
+ *_strlen - calculates the length of a string
+ *
+ *@s: the string to be counted
+ *
+ * Return: the length of the string
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
+/**
+ *_strcpy - copies a string pointed to by src
+ *
+ *@dest: destination
+ *@src:starting string point
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int p;
+	int i;
+
+	p = 0;
+
+
+	while (src[p] != '\0')
+	{
+		p++;
+	}
+
+	for (i = 0; i < p; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
 }
