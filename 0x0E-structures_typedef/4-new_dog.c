271cc97e03416dee6875324d5ec86e15496b773e
roundhousetally <tahlia.w.roper@gmail.com>
2020-03-10 03:06:41 +0000

    Task 4, 5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -23,7 +23,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (new_dog == NULL)
 	{
-		free(new_dog);
 		return (NULL);
 	}
 
@@ -45,7 +44,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog->owner);
-		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
