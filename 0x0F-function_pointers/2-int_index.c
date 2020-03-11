0fb1cbdf8d7be8978f92bcd1b0ae7c72cb2813f4
roundhousetally <tahlia.w.roper@gmail.com>
2020-03-11 18:57:35 +0000

    Simplified task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,15 +11,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL)
+	if (array == NULL || cmp == NULL)
 	{
 		return (-1);
 	}
 	if (size <= 0)
-	{
-		return (-1);
-	}
-	if (cmp == NULL)
 	{
 		return (-1);
 	}
