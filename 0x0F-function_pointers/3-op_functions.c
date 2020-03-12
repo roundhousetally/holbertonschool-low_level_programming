1ba7bb71236cd4c18cb0b2fff182fb136d368a9c
roundhousetally <tahlia.w.roper@gmail.com>
2020-03-12 02:24:08 +0000

    Op funcs

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -53,6 +53,7 @@ int op_div(int a, int b)
 	if (b == 0)
 	{
 		printf("Error\n");
+		exit(100);
 	}
 	return (a / b);
 }
@@ -69,6 +70,7 @@ int op_mod(int a, int b)
 	if (b == 0)
 	{
 		printf("Error\n");
+		exit(100);
 	}
 	return (a % b);
 }
