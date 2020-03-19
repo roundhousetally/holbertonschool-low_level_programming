428551c536333a0225dc0b3865ee4791ace2d592
roundhousetally <tahlia.w.roper@gmail.com>
2020-03-19 21:56:18 +0000

    Task 5 added header

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,8 +7,9 @@
  *
  * Return - void
  */
+
 void strtup(void)
 {
-	printf("You're beat! and yet, you must allow, \n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
