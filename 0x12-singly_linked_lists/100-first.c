7db455d4f9add00ab1e365d0a738441e611b0dac
roundhousetally <tahlia.w.roper@gmail.com>
2020-03-19 21:48:38 +0000

    Task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * strtup - starts before main
+ *
+ * Return - void
+ */
+void strtup(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
