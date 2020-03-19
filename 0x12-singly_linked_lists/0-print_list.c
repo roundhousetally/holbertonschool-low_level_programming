82d336ec0cfe72c607b4b914e7ccd34efbbbff36
roundhousetally <tahlia.w.roper@gmail.com>
2020-03-19 17:42:46 +0000

    Setup

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * print_list - prints all elements of a linked list.
+ *@h: the pointer to the linked list.
+ *Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+	int i;
+
+	i = h->len;
+
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)\n");
+		h = h->next;
+		count = 1;
+	}
+	while (h != NULL)
+	{
+		printf("[%i] %s\n", i, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
