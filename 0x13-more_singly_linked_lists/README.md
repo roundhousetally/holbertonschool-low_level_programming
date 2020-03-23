303ec3025c911fdd68f3af51d247d501284851c1
roundhousetally <tahlia.w.roper@gmail.com>
2020-03-23 14:41:51 +0000

    Setup header file and readme

---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,23 @@
+# More Singly Linked Lists
+
+### Task 0 - function that prints all the elements of an integer linked list.
+
+### Task 1 - function that returns the number of elements in a linked list.
+
+### Task 2 - function that adds a new node at the beginning of a linked list.
+
+### Task 3 - function that adds a new node at the end of a linked list.
+
+### Task 4 - function that frees a linked list.
+
+### Task 5 - function that frees a linked list with a double pointer.
+
+### Task 6 - function to delete the head of a node and returns the data.
+
+### Task 7 - function that returns the nth node of a linked list.
+
+### Task 8 - function that returns the sum of all the data of a linked list.
+
+### Task 9 - function that inserts a new node at a given position .
+
+### Task 10 - function that deletes the node at index.
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,59 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+/** 0 */
+size_t print_listint(const listint_t *h);
+
+/** 1 */
+size_t listint_len(const listint_t *h);
+
+/** 2 */
+listint_t *add_nodeint(listint_t **head, const int n);
+
+/** 3 */
+listint_t *add_nodeint_end(listint_t **head, const int n);
+
+/** 4 */
+void free_listint(listint_t *head);
+
+/** 5 */
+void free_listint2(listint_t **head);
+
+/** 6 */
+int pop_listint(listint_t **head);
+
+/** 7 */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+
+/** 8 */
+int sum_listint(listint_t *head);
+
+/** 9 */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+
+/** 10 */
+int delete_nodeint_at_index(listint_t **head, unsgined int index);
+
+
+
+
+
+
+#endif
