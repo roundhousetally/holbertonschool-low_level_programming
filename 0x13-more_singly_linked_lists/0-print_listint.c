a47decfefd14cecf400567b21afdd2225575ffcc
roundhousetally <tahlia.w.roper@gmail.com>
2020-03-23 15:17:26 +0000

    Task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints the elements in an iteger linked list
+ * @h: pointer to the head of the linked list
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (h != NULL)
+	{
+		printf("%u\n", h->n);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
