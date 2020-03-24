c7f0f829a05c293457b4199c645dfe877abde18f
roundhousetally <tahlia.w.roper@gmail.com>
2020-03-24 14:32:49 +0000

    Edge cases for 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,11 +11,11 @@ void free_listint2(listint_t **head)
 	listint_t *temp = *head;
 	listint_t *next;
 
-	while (temp != NULL)
+	while (temp != NULL && head != NULL)
 	{
+		temp = next;
 		next = temp->next;
 		free(temp);
-		temp = next;
 	}
 	*head = NULL;
 }
