342370361d86b38ba825dcf91fa8d09a214c39ba
roundhousetally <tahlia.w.roper@gmail.com>
2020-03-24 14:19:27 +0000

    Task 0 fix changed from u to d

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,11 +10,14 @@ size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
 
+	if (h == NULL)
+		return (0);
+
 	while (h != NULL)
 	{
-		printf("%u\n", h->n);
-		i++;
+		printf("%d\n", h->n);
 		h = h->next;
+		i++;
 	}
 	return (i);
 }
