01d3dff7bcb7176794d49545c7805e2461ac12d6
roundhousetally <tahlia.w.roper@gmail.com>
2020-03-26 19:34:49 +0000

    Task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_binary - prints the binary representation of an int
+ * @n: the number to convert
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
