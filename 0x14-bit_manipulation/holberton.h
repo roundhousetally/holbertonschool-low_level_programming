72173f9677c10ad26c7a5d1ed54ce18966853e7c
roundhousetally <tahlia.w.roper@gmail.com>
2020-03-26 19:36:00 +0000

    updated header

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -8,6 +8,7 @@ unsigned int binary_to_uint(const char *b);
 
 /** 1 */
 void print_binary(unsigned long int n);
+int _putchar(char c);
 
 /** 2 */
 int get_bit(unsigned long int n, unsigned int index);
