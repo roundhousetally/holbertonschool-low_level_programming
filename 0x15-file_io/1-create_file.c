0c319f5eb3c8abad6670d0b51d2f79178f1c142d
roundhousetally <tahlia.w.roper@gmail.com>
2020-03-31 20:01:28 +0000

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	opn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (opn == -1)
-		return (0);
+		return (-1);
 
 	if (text_content == NULL)
 		text_content = "";
