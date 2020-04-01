4d5fbd27a6cb1d52611eba3ff3879e7b6194bcfc
roundhousetally <tahlia.w.roper@gmail.com>
2020-04-01 01:09:16 +0000

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,23 +19,21 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
 			exit(97);
-	if (argv[1] == NULL) /* if no arg 1 */
+	if (argv[1] == NULL)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
 			argv[1]), exit(98);
-	if (argv[2] == NULL) /* if no arg 2 */
+	if (argv[2] == NULL)
 		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n",
 			argv[2]), exit(99);
 	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	rd = read(file_from, buffer, 1024);
+	if (rd == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
 			argv[1]), exit(98);
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (file_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n",
-		argv[2]), exit(99);
-	rd = read(file_from, buffer, 1024);
 	while (rd != 0)
-	{	wrt = write(file_to, buffer, rd);
+	{
+		wrt = write(file_to, buffer, rd);
 		if (wrt == -1 || rd != wrt)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
 				argv[2]), exit(99);
