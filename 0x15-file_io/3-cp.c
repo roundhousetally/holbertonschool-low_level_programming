383be47571cdb83b3e6bbdc2f4a4cde6055e13f4
roundhousetally <tahlia.w.roper@gmail.com>
2020-04-01 00:43:02 +0000

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,31 +17,32 @@ int main(int argc, char *argv[])
 	int file_from, file_to, rd, wrt;
 	char buffer[1024];
 
-	if (argc != 3) /* checking to make sure enough args */
+	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
 			exit(97);
-/**	if (argv[1] == NULL)
- *		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
- *			argv[1]), exit(98);
- *	if (argv[2] == NULL)
- *		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n",
- *			argv[2]), exit(99);
- */
-	 file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
-		       S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+			argv[1]), exit(98);
+
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n",
+			argv[2]), exit(99);
 
 	rd = read(file_from, buffer, 1024);
-	while (rd != 0) /* rd is not read only */
+	while (rd != 0)
 	{
-		wrt = write(file_to, buffer, rd);
-		if (wrt == -1 || rd != wrt) /* if write fails */
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
-				argv[2]), exit(99);
 		rd = read(file_from, buffer, 1024);
 		if (rd == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from %s\n",
 				argv[1]), exit(98);
+
+		wrt = write(file_to, buffer, rd);
+		if (wrt == -1 || rd != wrt)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
+				argv[2]), exit(99);
 	}
 	wrt = close(file_from);
 	if (wrt == -1)
