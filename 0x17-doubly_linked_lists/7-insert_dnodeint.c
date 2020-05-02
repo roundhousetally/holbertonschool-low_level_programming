295dfb25474418db5f9abdf8b7db1ad29b0b689a
roundhousetally <tahlia.w.roper@gmail.com>
2020-05-02 20:05:04 +0000

    Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,24 +13,27 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *nn, *next1, *temp = *h;
 	unsigned int i = 0;
 
-	if (h == NULL && idx != 0)
+	if (h == NULL)
 		return (NULL);
 	next1 = *h;
 	nn = malloc(sizeof(dlistint_t));
 	if (nn == NULL)
 		return (NULL);
 	nn->n = n;
+	if ((temp == NULL) && (idx == 0))
+	{
+		nn->prev = NULL, nn->next = NULL, *h = nn;
+		return (nn);
+	}
 	if (idx == 0)
 	{
 		if (next1 != NULL)
 		{
-			nn->next = next1;
-			next1->prev = nn;
+			nn->next = next1, next1->prev = nn;
 		}
 		else
 			nn->next = NULL;
-		nn->prev = NULL;
-		*h = nn;
+		nn->prev = NULL, *h = nn;
 		return (*h);
 	}
 	while (i < (idx - 1))
@@ -43,10 +46,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		next1 = next1->next;
 		i++;
 	}
-	temp = next1;
-	next1 = next1->next;
-	temp->next = nn;
-	nn->next = next1;
+	temp = next1, next1 = next1->next;
+	temp->next = nn, nn->next = next1;
 	nn->prev = temp;
 	return (nn);
 }
