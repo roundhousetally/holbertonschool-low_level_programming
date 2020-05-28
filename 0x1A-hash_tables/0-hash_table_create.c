becc9305fd1720baef73e3a9cf5a39606eab9167
roundhousetally <tahlia.w.roper@gmail.com>
2020-05-28 21:36:06 +0000

    first commit

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a new hash table
+ * @size: size of table
+ * Return: pointer to new hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *newtable;
+	unsigned int i;
+
+	/** if size too small, no table bby */
+	if(size < 1)
+	{
+		return (NULL);
+	}
+	newtable = malloc(sizeof(hash_table_t));
+	/** allocate for table */
+	if (newtable == NULL)
+	{
+		return (NULL);
+	}
+	/** allocate for entries */
+	newtable->array = malloc(sizeof(hash_node_t *) * size);
+	if(newtable->array == NULL)
+	{
+		return(NULL);
+	}
+	/** set table to NULL and return new table */
+	for (i = 0; i < size; i++)
+	{
+		newtable->array[i] = NULL;
+	}
+	return (newtable);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,15 @@
+# C - Hash Tables
+
+### Task 0 - Function that creates a hash table
+
+### Task 1 - Function implementing the djb2 algorithm
+
+### Task 2 - Function that gives an index of a key
+
+### Task 3 - Function that adds an element to the hash table
+
+### Task 4 - Function that retrieves a value associated with a key
+
+### Task 5 - Function that prints a hash table
+
+### Task 6 - Function that deletes a hash table
