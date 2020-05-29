c5a85f6395be055cbf37ea6671eaf76c26a2e84a
roundhousetally <tahlia.w.roper@gmail.com>
2020-05-29 19:49:58 +0000

    5, commas and stop

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -10,6 +10,7 @@ void hash_table_print(const hash_table_t *ht)
 {
 	const hash_table_t *look;
 	unsigned int i = 0;
+	int comstop = 1;
 
 	look = ht;
 
@@ -23,10 +24,20 @@ void hash_table_print(const hash_table_t *ht)
 		}
 		while (look->array[i] != NULL)
 		{
-			printf("'%s': '%s'", look->array[i]->key, look->array[i]->value);
+			if (!comstop)
+			{
+				printf(", ");
+				printf("'%s': '%s'", look->array[i]->key,
+				       look->array[i]->value);
+			}
+			else
+			{
+				printf("'%s': '%s'", look->array[i]->key,
+				       look->array[i]->value);
+				comstop = 0;
+			}
 			look->array[i] = ((look->array[i])->next);
 		}
-
 	}
 	printf("}");
 	printf("\n");
